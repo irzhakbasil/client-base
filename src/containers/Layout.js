794e7128704335cff5a24c9b36b0cc0293b4559e
marvin <irzhakbasil@gmail.com>
2018-11-24 00:41:22 +0200

    layout css made

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
 
+import "./Layout.css";
+
+import SearchBar from "../components/SearchBar/SearchBar";
+import ClientList from "../components/ClientsList/ClientsList";
+import ClientInfo from "../components/ClientInfo/ClientInfo";
+
 class Layout extends Component {
   render() {
-    return <div className="App">HELLO</div>;
+    return (
+      <div className="layout">
+        <div className="left-panel">
+          <SearchBar />
+          <ClientList />
+        </div>
+        <div className="right-panel">
+          <ClientInfo />
+        </div>
+      </div>
+    );
   }
 }
 
