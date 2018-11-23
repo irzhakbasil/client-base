755d7fd5e1bca31da7bd63a3b722c935842b6b2f
marvin <irzhakbasil@gmail.com>
2018-11-24 01:18:10 +0200

    redux setup is done

--- src/index.js
+++ src/index.js
@@ -8,8 +8,16 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import reducer from "./store/reducers";
 
-ReactDOM.render(<Layout />, document.getElementById("root"));
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Layout />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/store/reducers/clientReducer.js
@@ -0,0 +1,5 @@
+const initialState = {};
+
+export default function clientReducer(state = initialState, action) {
+  return state;
+}
